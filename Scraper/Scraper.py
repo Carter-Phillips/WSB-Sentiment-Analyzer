4786d6d432043c964e694f62ba27c873209caeef
Carter Phillips <carter.joel.phillips@gmail.com>
2021-09-15 22:15:54 -0700

    scraping functioning and used in main

--- Scraper/Scraper.py
+++ Scraper/Scraper.py
@@ -11,26 +11,18 @@ def scrape(subreddit):
     with open('./credentials.json') as jsonFile:
         data = json.load(jsonFile)
 
-    ##create the reddit client using our credentials file
+    # create the reddit client using our credentials file
     reddit = praw.Reddit(client_id=data['credentials']['client_id'],
                          client_secret=data['credentials']['client_secret'],
                          user_agent=data['credentials']['user_agent'],
                          username=data['credentials']['username'],
                          password=data['credentials']['password'])
-    pushApi = PushshiftAPI()
 
-    ## get all posts for the last day that have the '$' character in them and have the due dilligence tag
-
-    #epoch for 1 day ago
-    start_epoch = int((datetime.today() - timedelta(days=1)).timestamp())
-
-    #get list of posts
-    posts = (pushApi.search_submissions(after=start_epoch, subreddit=subreddit))
-
-
-    #innit posts
-    #posts will have the post text, upvotes, comments, and their upvotes
-    # posts[0] = {text: string, upvotes: number, comments: {text: string, upvotes: number, responses: {commentsObject}[]}}[]
+    # innit posts
     posts = []
 
-    print("Posts:", posts)
+    # get top 100 posts of the day
+    for submission in reddit.subreddit(subreddit).top(time_filter='day'):
+        posts.append(submission)
+
+    return posts
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-import os
+from Scraper.Scraper import scrape
 
+
+def main():
+
+    print("Getting top posts of day...")
+    posts = scrape('wallstreetbets')
+    print('Posts fetched')
+
+
+if __name__ == '__main__':
+
+    main()
